4cf55b6e25ebbb90de1ba5dc3adccfc0397bb474
Léo Pradel <pradel.leo@gmail.com>
2023-02-13 14:55:50 +0100

    feat: setup mantine ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+        primaryColor: "orange",
+        fontFamily: inter.style.fontFamily,
+        headings: { fontFamily: inter.style.fontFamily },
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
